44653e9a0757f31b25fc1b883c97df3c03c0606a
Memo Akten <memo@memo.tv>
2013-01-08 22:08:33 +0200

    example window height

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -6,7 +6,7 @@ int main( ){
     
     // can be OF_WINDOW or OF_FULLSCREEN
     // pass in width and height too:
-    ofSetupOpenGL(1100,600, OF_WINDOW);            // <-------- setup the GL context
+    ofSetupOpenGL(1100, 720, OF_WINDOW);            // <-------- setup the GL context
     
     // this kicks off the running of my app
     ofRunApp(new testApp);
